25368b320baf99b01839cb7cebc2ba409876caf5
jae kim <jaehkim23@gmail.com>
2025-05-18 19:06:38 -0500

    updated api for production

--- src/app/reviews/page.js
+++ src/app/reviews/page.js
@@ -7,7 +7,7 @@ export const metadata = {
 };
 
 async function fetchReviews () {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/reviews`, {
+    const response = await fetch('/api/reviews', {
         cache:'no-store'
     });
 
